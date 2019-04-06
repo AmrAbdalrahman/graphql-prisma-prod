2225b435f0714173400199f0edbe64015bfac386
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-04-06 21:38:39 +0200

    add secret key to heroku prod

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,7 @@ import { fragmentReplacements } from './resolvers/index'
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: 'magicanosecrettest',
+    secret: process.env.PRISMA_SECRET,
     fragmentReplacements
 });
 
